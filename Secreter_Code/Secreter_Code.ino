69531b23f4281c3d6b37ea7299226b8280299a0d
James Bickerdike <18BickJE@student.cgs.vic.edu.au>
2015-12-20 22:46:46 +1100

    Further improvements
    Shortened code using an array

--- Secreter_Code/Secreter_Code.ino
+++ Secreter_Code/Secreter_Code.ino
@@ -3,6 +3,8 @@ boolean incorrect = false;  //for detecting an error
 int lightA = 12;  //output light pins
 int lightB = 11;
 
+int option[] = {3, 2, 2, 2, 3};
+
 int buttonA = 3;  //input button pins
 int buttonB = 2;
 
@@ -16,96 +18,41 @@ void setup() {
   pinMode(buttonB, INPUT);
 }
 
-void loop() {
-  while((digitalRead(buttonA) == LOW) && (digitalRead(buttonB) == LOW)) //while loop continuosly delays until a button is pressed
+void(*resetFunc)(void) = 0;
+
+void loop() 
+{
+  for(int i = 0; i < 5; i++)
   {
-    delay(waitDelay);
-  }
-  if(digitalRead(buttonA) == HIGH)  //if buttonA is pressed
-  {
-    digitalWrite(lightA, HIGH); //blink lightA
-    delay(flashDelay);
-    digitalWrite(lightA, LOW);
-    while((digitalRead(buttonA) == LOW) && (digitalRead(buttonB) == LOW)) //waits again until a button is pressed
+    while((digitalRead(buttonA) == LOW) && (digitalRead(buttonB) == LOW)) //while loop continuosly delays until a button is pressed
     {
       delay(waitDelay);
     }
-    if(digitalRead(buttonB) == HIGH)  //this time if buttonB is pressed. This if is within the other if, meaning they both have to occur in the right order
+    if(digitalRead(option[i]) == HIGH)  //if buttonA is pressed
     {
-      digitalWrite(lightB, HIGH); //flash lightB
+      digitalWrite((option[i] + 9), HIGH); //blink lightA
       delay(flashDelay);
-      digitalWrite(lightB, LOW);
-      while((digitalRead(buttonA) == LOW) && (digitalRead(buttonB) == LOW)) //does the cycle again with different buttons/lights
-      {
-        delay(waitDelay);
-      }
-      if(digitalRead(buttonB) == HIGH)
-      {
-        digitalWrite(lightB, HIGH);
-        delay(flashDelay);
-        digitalWrite(lightB, LOW);
-        while((digitalRead(buttonA) == LOW) && (digitalRead(buttonB) == LOW))
-        {
-          delay(waitDelay);
-        }
-        if(digitalRead(buttonB) == HIGH)
-        {
-          digitalWrite(lightB, HIGH);
-          delay(flashDelay);
-          digitalWrite(lightB, LOW);
-          while((digitalRead(buttonA) == LOW) && (digitalRead(buttonB) == LOW))
-          {
-            delay(waitDelay);
-          }
-          if(digitalRead(buttonA) == HIGH)
-          {
-            digitalWrite(lightA, HIGH);
-            delay(flashDelay);
-            digitalWrite(lightA, LOW);
-            delay(100);
-            digitalWrite(lightA, HIGH); //once enough if's have been accepted, both lights will turn on
-            digitalWrite(lightB, HIGH);
-            for(int i = 1; i > 0; i++)  //infinate loop
-            {
-              delay(10);
-            }
-          }
-          else  //all the of the elses are linked to previous if statements. if any ifs are not true, then they will end up in one of these
-          {
-            incorrect = true; //indicates that the user has made an error for later coding
-          }
-        }
-        else
-        {
-          incorrect = true;
-        }
-      }
-      else
-      {
-        incorrect = true;
-      }
+      digitalWrite((option[i] + 9), LOW);
     }
     else
     {
-      incorrect = true;
+      for(int i = 1; i < 20; i++) //for loop, running 20 times
+      {
+        digitalWrite(lightA, HIGH);//blinks both lights quickly 20 times
+        digitalWrite(lightB, HIGH);
+        delay(40);
+        digitalWrite(lightA, LOW);
+        digitalWrite(lightB, LOW);
+        delay(40);
+      }
+      resetFunc();
     }
-  }
-  else
+  }  
+  digitalWrite(lightA, HIGH); //once enough if's have been accepted, both lights will turn on
+  digitalWrite(lightB, HIGH);
+  for(int i = 1; i > 0; i++)  //infinate loop
   {
-    incorrect = true;
-  }
-  if(incorrect == true) //if the user has made an error, or an if statement is not true
-  {
-    for(int i = 1; i < 20; i++) //for loop, running 20 times
-    {
-      digitalWrite(lightA, HIGH);//blinks both lights quickly 20 times
-      digitalWrite(lightB, HIGH);
-      delay(40);
-      digitalWrite(lightA, LOW);
-      digitalWrite(lightB, LOW);
-      delay(40);
-    }
-    incorrect = false;  //resets the incorrect if statements
+    delay(10);
   }
 }
  
